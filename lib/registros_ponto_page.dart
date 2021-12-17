e9c70dcf29f42a58e92700be2a713c64cca3fd9b
filipe.jesus <33381421+filipepereir@users.noreply.github.com>
2021-12-17 11:21:40 -0300

    Corrigindo lista de registros
    Não estava renderizando a lista ao abrir a página de registros.

--- lib/registros_ponto_page.dart
+++ lib/registros_ponto_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:grouped_list/grouped_list.dart';
 import 'package:workspace_flutter/service/PontoService.dart';
+import 'package:workspace_flutter/utils/loader.dart';
 
 import 'dto/RegistrosDTO.dart';
 
@@ -15,94 +16,108 @@ class Registros extends StatefulWidget {
 class _RegistrosState extends State<Registros> {
   List<RegistrosDTO> registros = [];
 
+  PontoService pontoService = PontoService();
+
+  bool loader = false;
+
   @override
   void initState() {
-    PontoService pontoService = PontoService();
-    print("INIT");
     super.initState();
-
-    pontoService.buscarRegistros().then(
-          (value) => {
-            registros = value,
-          },
-        );
+    loader = true;
+    pontoService.buscarRegistros().then((value) {
+      setState(
+        () {
+          registros = value;
+        },
+      );
+    }).whenComplete(
+      () => {
+        loader = false,
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //color: Colors.red,
-      child: GroupedListView<RegistrosDTO, String>(
-        elements: registros,
-        groupBy: (element) => element.dataBatida,
-        groupComparator: (value1, value2) => value2.compareTo(value1),
-        itemComparator: (item1, item2) =>
-            item1.nomeUsuario.compareTo(item2.nomeUsuario),
-        order: GroupedListOrder.DESC,
-        groupSeparatorBuilder: (String value) => Material(
-          elevation: 3,
-          child: Container(
-            color: Colors.white,
-            width: MediaQuery.of(context).size.width * 0.99,
-            padding: const EdgeInsets.all(10),
-            margin: const EdgeInsets.all(0),
-            child: Text(
-              value,
-              style: const TextStyle(fontSize: 20, color: Color(0xFF707070)),
+    if (loader) {
+      return Container(
+        alignment: Alignment.center,
+        child: Loader(),
+      );
+    } else {
+      return Container(
+        //color: Colors.red,
+        child: GroupedListView<RegistrosDTO, String>(
+          elements: registros,
+          groupBy: (element) => element.dataBatida,
+          groupComparator: (value1, value2) => value2.compareTo(value1),
+          itemComparator: (item1, item2) =>
+              item1.nomeUsuario.compareTo(item2.nomeUsuario),
+          order: GroupedListOrder.ASC,
+          groupSeparatorBuilder: (String value) => Material(
+            elevation: 3,
+            child: Container(
+              color: Colors.white,
+              width: MediaQuery.of(context).size.width * 0.99,
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.all(0),
+              child: Text(
+                value,
+                style: const TextStyle(fontSize: 20, color: Color(0xFF707070)),
+              ),
             ),
           ),
-        ),
 
-        itemBuilder: (c, element) {
-          return Column(
-            children: [
-              Container(
-                //color: Colors.blueGrey[50],
-                width: MediaQuery.of(context).size.width * 0.99,
-                //padding: EdgeInsets.all(10),
-                padding: const EdgeInsets.only(
-                    left: 20, bottom: 10, right: 20, top: 10),
-                margin: const EdgeInsets.all(0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        FittedBox(
-                          child: Text(
-                            element.status,
-                            style: const TextStyle(
-                              color: Color(0xFF003E51),
-                              fontSize: 18,
+          itemBuilder: (c, element) {
+            return Column(
+              children: [
+                Container(
+                  //color: Colors.blueGrey[50],
+                  width: MediaQuery.of(context).size.width * 0.99,
+                  //padding: EdgeInsets.all(10),
+                  padding: const EdgeInsets.only(
+                      left: 20, bottom: 10, right: 20, top: 10),
+                  margin: const EdgeInsets.all(0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          FittedBox(
+                            child: Text(
+                              element.status,
+                              style: const TextStyle(
+                                color: Color(0xFF003E51),
+                                fontSize: 18,
+                              ),
                             ),
                           ),
-                        ),
-                        FittedBox(
-                          child: Text(
-                            //widget.descricaoPapel,
-                            element.dataHoraRegistro,
-                            style: const TextStyle(
-                                color: Color(0xFF707070),
-                                fontSize: 15,
-                                fontFamily: 'Arial'),
+                          FittedBox(
+                            child: Text(
+                              //widget.descricaoPapel,
+                              element.dataHoraRegistro,
+                              style: const TextStyle(
+                                  color: Color(0xFF707070),
+                                  fontSize: 15,
+                                  fontFamily: 'Arial'),
+                            ),
                           ),
-                        ),
-                        FittedBox(
-                          child: Text(
-                            //widget.descricaoPapel,
-                            element.mensagem,
-                            style: const TextStyle(
-                                color: Color(0xFF707070),
-                                fontSize: 15,
-                                fontFamily: 'Arial'),
+                          FittedBox(
+                            child: Text(
+                              //widget.descricaoPapel,
+                              element.mensagem,
+                              style: const TextStyle(
+                                  color: Color(0xFF707070),
+                                  fontSize: 15,
+                                  fontFamily: 'Arial'),
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: const [
-                        /*
+                        ],
+                      ),
+                      Row(
+                        children: const [
+                          /*
                         InkWell(
                           onTap: () {},
                           child: Image.asset(
@@ -113,19 +128,20 @@ class _RegistrosState extends State<Registros> {
                         ),
 
                          */
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              const Divider(
-                color: Colors.black87,
-              )
-            ],
-          );
-        },
-        // useStickyGroupSeparators: true,
-      ),
-    );
+                const Divider(
+                  color: Colors.black87,
+                )
+              ],
+            );
+          },
+          // useStickyGroupSeparators: true,
+        ),
+      );
+    }
   }
 }
